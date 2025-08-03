c08e57d3d3625dc5e424112bf12f3b1ba62d892d
Elizaveta Tikhonova <tihonova.e@medindex.ru>
2025-08-03 14:54:35 +0300

    second commit

---
+++ test.py
@@ -0,0 +1,97 @@
+import pytest
+from main import BooksCollector
+
+@pytest.fixture()
+def collector():
+    return BooksCollector()
+
+def test_add_new_book_valid_len_name(collector):
+    
+    book_name = "Война и мир"
+    collector.add_new_book(book_name)
+    lst_book = collector.get_books_genre().keys() 
+    assert book_name in lst_book
+     
+
+def test_set_book_genre_with_valid_genre(collector):
+    book_name = 'Война и Мир'
+    genre = 'Ужасы'
+    collector.add_new_book(book_name)
+    collector.set_book_genre(book_name, genre)
+    dict_book = collector.get_books_genre()
+    assert dict_book[book_name] == genre
+
+
+def test_get_book_genre_get_by_name(collector):
+    book_name = 'Гарри Поттер'
+    genre = 'Фантастика'
+    collector.add_new_book(book_name)
+    collector.set_book_genre(book_name, genre)
+    genre_book = collector.get_book_genre(book_name)
+    assert genre_book == genre
+
+
+def test_get_books_with_specific_genre_empty_collector(collector):
+    assert collector.get_books_with_specific_genre('Фантастика') == []
+
+
+def test_get_books_with_specific_genre_one_book(collector):   
+    book_name = 'Гарри Поттер'
+    genre = 'Фантастика'
+    collector.add_new_book(book_name)
+    collector.set_book_genre(book_name, genre)
+    assert collector.get_books_with_specific_genre('Фантастика') == ['Гарри Поттер'] 
+    
+
+def test_get_books_genre_with_multiple_books(collector):
+    collector.add_new_book('Война и мир')
+    collector.set_book_genre('Война и мир', 'Детективы')
+
+    collector.add_new_book('Гарри Поттер')
+    collector.set_book_genre('Гарри Поттер', 'Фантастика')
+
+    collector.add_new_book('Холодное сердце')
+    collector.set_book_genre('Холодное сердце', 'Мультфильмы')
+    
+    assert collector.get_books_genre() == {
+            'Война и мир': 'Детективы',
+            'Гарри Поттер': 'Фантастика',
+            'Холодное сердце': 'Мультфильмы'}
+        
+
+def test_get_books_for_children_no_children_books(collector):
+    collector.add_new_book('Звонок')
+    collector.set_book_genre('Звонок', 'Ужасы')   
+
+    collector.add_new_book('Шерлок Холмс')
+    collector.set_book_genre('Шерлок Холмс', 'Детективы') 
+    
+    assert collector.get_books_for_children() == [] 
+
+
+def test_add_book_in_favorites_new_book(collector):
+    
+    book_name = 'Шерлок Холмс'
+    genre = 'Детективы'
+    collector.add_new_book(book_name)
+    collector.set_book_genre(book_name, genre)
+    collector.add_book_in_favorites(book_name)
+    lst_favorites = collector.get_list_of_favorites_books()
+    assert book_name in lst_favorites
+
+
+def test_delete_existing_book_from_favorites(collector):
+    
+    book_name = 'Шерлок Холмс'
+    genre = 'Детективы'
+    collector.add_new_book(book_name)
+    collector.set_book_genre(book_name, genre)
+    collector.add_book_in_favorites(book_name)
+    collector.delete_book_from_favorites(book_name)
+    assert book_name not in collector.get_list_of_favorites_books()
+
+    
+def test_get_list_of_favorites_books_get_empty_favorites_list(collector):
+    
+    assert collector.get_list_of_favorites_books() == []
+
